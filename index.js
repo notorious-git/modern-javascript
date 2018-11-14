42a65fb40ead9d498230241dfc2f8b681ee43866
Peter Jang <peter.x.jang@gmail.com>
2018-11-13 19:05:22 -0600

    Add another package as an example

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var moment = require("moment");
 var numeral = require("numeral");
+var math = require("mathjs");
 
 console.log("Hello from JavaScript!");
 var datetime = moment()
@@ -12,3 +13,5 @@ var value2 = myNumeral2.value();
 console.log("the value is", value2);
 
 console.log("YO!!!");
+
+console.log(math.sqrt(-4));

4de0c6b19108d3cc4a26dd66b39b2a95c972c5e0
Peter Jang <peter.x.jang@gmail.com>
2018-11-13 18:50:56 -0600

    Add another example package

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/* global moment */
+/* global moment, numeral */
 
 console.log("Hello from JavaScript!");
 var datetime = moment()
   .startOf("day")
   .fromNow();
 console.log(datetime);
+
+var myNumeral2 = numeral("1,000");
+var value2 = myNumeral2.value();
+console.log("the value is", value2);

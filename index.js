1a457c2261dd07a90b74804666103b35c89952dc
Peter Jang <peter.x.jang@gmail.com>
2018-11-13 19:00:10 -0600

    Use webpack to create a single bundle

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/* global moment, numeral */
+var moment = require("moment");
+var numeral = require("numeral");
 
 console.log("Hello from JavaScript!");
 var datetime = moment()
@@ -9,3 +10,5 @@ console.log(datetime);
 var myNumeral2 = numeral("1,000");
 var value2 = myNumeral2.value();
 console.log("the value is", value2);
+
+console.log("YO!!!");
